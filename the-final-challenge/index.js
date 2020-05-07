8d6559826325dad8f05e698de0482609d4021455
Martin Fencl <martin.@layeredcode.com>
2020-05-06 21:57:01 -0500

    refactor add/remove shadow js, rename variable, methods

--- the-final-challenge/index.js
+++ the-final-challenge/index.js
@@ -8,29 +8,32 @@ const get = (elementId) => document.getElementById(elementId)
 const btnOpenNav  = get('btn-open-nav')
 const btnCloseNav = get('btn-close-nav')
 const header      = get('header')
-const navHeader   = get('nav-header')
+const nav         = get('nav-header')
 
-let shadowTimeout
+let expireHeaderNavShadow
 
-const addTimedOutShadow = () => {
+const addExpiringHeaderNavShadow = () => {
+  clearTimeout(expireHeaderNavShadow)
+  addHeaderNavShadow()
+  expireHeaderNavShadow = setTimeout(() => removeHeaderNavShadow(), 150)
+}
+
+const addHeaderNavShadow = () => {
   header.classList.add('box-shadow')
-  if (innerWidth <= 700) { navHeader.classList.add('box-shadow') }
-
-  clearTimeout(shadowTimeout)
-  shadowTimeout = setTimeout(() => removeShadow(), 150)
+  if (innerWidth <= 700) { nav.classList.add('box-shadow') }
 }
 
-const removeShadow = () => {
+const removeHeaderNavShadow = () => {
   header.classList.remove('box-shadow')
-  navHeader.classList.remove('box-shadow')
+  nav.classList.remove('box-shadow')
 }
 
-const toggleNav = () => navHeader.classList.toggle('display-nav')
+const toggleNav = () => nav.classList.toggle('display-nav')
 
 // Apparently & interestingly:
 //  if no recepient is provided for a method call
 //  then `window` would be the default recipient:
-window.addEventListener('scroll', () => addTimedOutShadow())
+window.addEventListener('scroll', () => addExpiringHeaderNavShadow())
 
 btnOpenNav.addEventListener('click', () => toggleNav())
 btnCloseNav.addEventListener('click', () => toggleNav())
